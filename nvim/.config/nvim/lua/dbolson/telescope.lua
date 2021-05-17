7626950c13b5600733b07b798df42a64f341f1ce
Danny Olson <dbolson@gmail.com>
2021-05-16 17:09:07 -0700

    Formatting

--- nvim/.config/nvim/lua/dbolson/telescope.lua
+++ nvim/.config/nvim/lua/dbolson/telescope.lua
@@ -10,11 +10,11 @@ require('telescope').setup {
       '--with-filename'
     }
   },
-    extensions = {
-        fzy_native = {
-            override_generic_sorter = false,
-            override_file_sorter = true,
-        }
+  extensions = {
+    fzy_native = {
+      override_generic_sorter = false,
+      override_file_sorter = true,
     }
+  }
 }
 require('telescope').load_extension('fzy_native')
