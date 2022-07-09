d55cdd6fa1e76ee3d7221cc555be32182295e1c7
Danny Olson <dolson@launchdarkly.com>
2022-07-09 09:12:09 -0700

    Formatting and show relative paths in lualine

--- nvim/.config/nvim/lua/plugins/misc.lua
+++ nvim/.config/nvim/lua/plugins/misc.lua
@@ -1,42 +1,42 @@
-local navic = require('nvim-navic')
-require('fidget').setup()
-require('gitsigns').setup({
-  signs = {
-    add = {hl = 'GitSignsAdd', text = '+', numhl='GitSignsAddNr', linehl='GitSignsAddLn'},
-    change = {hl = 'GitSignsChange', text = '│', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
-    delete = {hl = 'GitSignsDelete', text = '_', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
-  }
-})
-require('lint').linters_by_ft = {
-  python = {'flake8'},
-  sh = {'shellcheck'},
-}
-local shellcheck = require('lint.linters.shellcheck')
-table.insert(shellcheck.args, '-x')
-
-require('lualine').setup({
-  options = {
-    theme = 'gruvbox_dark',
-  },
-  sections = {
-    lualine_c = {
-      { navic.get_location, cond = navic.is_available },
+local navic = require("nvim-navic")
+require("fidget").setup()
+require("gitsigns").setup({
+    signs = {
+        add = {
+            hl = "GitSignsAdd",
+            text = "+",
+            numhl = "GitSignsAddNr",
+            linehl = "GitSignsAddLn",
+        },
+        change = {
+            hl = "GitSignsChange",
+            text = "│",
+            numhl = "GitSignsChangeNr",
+            linehl = "GitSignsChangeLn",
+        },
+        delete = {
+            hl = "GitSignsDelete",
+            text = "_",
+            numhl = "GitSignsDeleteNr",
+            linehl = "GitSignsDeleteLn",
+        },
     },
-    lualine_x = {'filename', 'fileformat', 'filetype'},
-  }
+})
+require("lint").linters_by_ft = {python = {"flake8"}, sh = {"shellcheck"}}
+local shellcheck = require("lint.linters.shellcheck")
+table.insert(shellcheck.args, "-x")
+
+require("lualine").setup({
+    options = {theme = "gruvbox_dark"},
+    sections = {
+        lualine_c = {{navic.get_location, cond = navic.is_available}},
+        lualine_x = {{"filename", path = 1}, "fileformat", "filetype"},
+    },
 })
 
-require('nvim_comment').setup({
-  comment_empty = false,
-  create_mappings = false,
-})
-require('trouble').setup({
-  mode = 'document_diagnostics',
-})
-require('which-key').setup()
+require("nvim_comment").setup({comment_empty = false, create_mappings = false})
+require("trouble").setup({mode = "document_diagnostics"})
+require("which-key").setup()
 
 -- symbols_outline.nvim
-vim.g.symbols_outline = {
-  auto_preview = false,
-  width = 20,
-}
+vim.g.symbols_outline = {auto_preview = false, width = 20}
