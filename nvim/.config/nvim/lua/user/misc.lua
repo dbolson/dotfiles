c93b688c74c447c6bf684a1433d7f37f61d22ae9
Danny Olson <dolson@launchdarkly.com>
2022-08-11 13:22:17 -0700

    Formatting

--- nvim/.config/nvim/lua/user/misc.lua
+++ nvim/.config/nvim/lua/user/misc.lua
@@ -2,7 +2,12 @@ local navic = require("nvim-navic")
 
 require("gitsigns").setup({
     signs = {
-        add = {hl = "GitSignsAdd", text = "+", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn"},
+        add = {
+            hl = "GitSignsAdd",
+            text = "+",
+            numhl = "GitSignsAddNr",
+            linehl = "GitSignsAddLn",
+        },
         change = {
             hl = "GitSignsChange",
             text = "│",
@@ -17,18 +22,48 @@ require("gitsigns").setup({
         },
     },
 })
-require("lint").linters_by_ft = {go = {"golangcilint"}, python = {"flake8"}, sh = {"shellcheck"}}
+require("lint").linters_by_ft = {
+    go = {
+        "golangcilint",
+    },
+    python = {
+        "flake8",
+    },
+    sh = {
+        "shellcheck",
+    },
+}
 local shellcheck = require("lint.linters.shellcheck")
 table.insert(shellcheck.args, "-x")
 require("lualine").setup({
-    options = {theme = "gruvbox_dark"},
+    options = {
+        theme = "gruvbox_dark",
+    },
     sections = {
-        lualine_c = {{navic.get_location, cond = navic.is_available}},
-        lualine_x = {{"filename", path = 1}, "fileformat", "filetype"},
+        lualine_c = {
+            {
+                navic.get_location,
+                cond = navic.is_available,
+            },
+        },
+        lualine_x = {
+            {
+                "filename",
+                path = 1,
+            },
+            "fileformat",
+            "filetype",
+        },
     },
 })
-require("nvim_comment").setup({comment_empty = false, create_mappings = false})
+require("nvim_comment").setup({
+    comment_empty = false,
+    create_mappings = false,
+})
 require("which-key").setup()
 
 -- symbols_outline.nvim
-vim.g.symbols_outline = {auto_preview = false, width = 20}
+vim.g.symbols_outline = {
+    auto_preview = false,
+    width = 20,
+}
