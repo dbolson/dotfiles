0493c689ff0b2a9be0d023c3e7e6115462be51d0
Danny Olson <dolson@launchdarkly.com>
2022-09-20 13:11:55 -0700

    Show diagnostics in nvim-tree

--- nvim/.config/nvim/lua/user/tree.lua
+++ nvim/.config/nvim/lua/user/tree.lua
@@ -1,7 +1,18 @@
 require("nvim-tree").setup({
-    disable_netrw = false,
-    filters = {
-        custom = {nvim_tree_ignore = {".pytest_cache", "__pycache__", "node_modules", "vendor"}},
+    diagnostics = {
+        enable = true,
     },
-    renderer = {add_trailing = true},
+    filters = {
+        custom = {
+            nvim_tree_ignore = {
+                ".pytest_cache",
+                "__pycache__",
+                "node_modules",
+                "vendor",
+            },
+        },
+    },
+    renderer = {
+        add_trailing = true,
+    },
 })
