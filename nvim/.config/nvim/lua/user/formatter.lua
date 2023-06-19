595327834bb71911d924cb43278ee9ba478190f3
Danny Olson <dolson@launchdarkly.com>
2023-06-19 14:54:53 -0700

    Remove sql formatter

--- nvim/.config/nvim/lua/user/formatter.lua
+++ nvim/.config/nvim/lua/user/formatter.lua
@@ -81,17 +81,5 @@ require("formatter").setup({
                 }
             end,
         },
-        sql = {
-            function()
-                return {
-                    exe = "sql-formatter",
-                    args = {
-                        "--config ~/sql-formatter.json",
-                        util.escape_path(util.get_current_buffer_file_path()),
-                    },
-                    stdin = true,
-                }
-            end,
-        },
     },
 })
