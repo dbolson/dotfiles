7a50f0ca3661d667ef2a1b748790562fbc70a37c
Danny Olson <dbolson@gmail.com>
2026-02-26 10:28:19 -0800

    Update wilder

--- nvim/.config/nvim/lua/user/wilder.lua
+++ nvim/.config/nvim/lua/user/wilder.lua
@@ -6,6 +6,13 @@ wilder.setup({
     "?",
   },
 })
-wilder.set_option("renderer", wilder.popupmenu_renderer({
-  highlighter = wilder.basic_highlighter(),
-}))
+wilder.set_option('renderer', wilder.popupmenu_renderer(
+  wilder.popupmenu_palette_theme({
+    border = 'single',
+    left = {' ', wilder.popupmenu_devicons()},
+    max_height = '50%',
+    min_height = 0,
+    prompt_position = 'top',
+    reverse = 0,
+  })
+))
