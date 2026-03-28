021e764a95e795df1f92002f1d10921a7b2fc953
Danny Olson <dbolson@gmail.com>
2026-03-28 09:04:31 -0700

    Remove extra treesitter file

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,24 +1,26 @@
 return {
-  {
-    'nvim-treesitter/nvim-treesitter',
-    branch = 'main',
-    build = ':TSUpdate',
-    lazy = false,
-    opts = {
-      ensure_installed = {
-        'c',
-        'eex',
-        'elixir',
-        'erlang',
-        'go',
-        'lua',
-        'markdown',
-        'markdown_inline',
-        'python',
-        'query',
-        'typescript',
-      },
-    },
-    -- tag = 'v0.10.0',
-  },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		branch = "main",
+		build = ":TSUpdate",
+		lazy = false,
+		opts = {
+			install = {
+				"eex",
+				"elixir",
+				"erlang",
+				"go",
+				"html",
+				"javascript",
+				"lua",
+				"markdown",
+				"markdown_inline",
+				"python",
+				"query",
+				"typescript",
+				"yaml",
+			},
+		},
+		-- tag = 'v0.10.0',
+	},
 }
